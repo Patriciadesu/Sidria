b65165dd1c7246aba5036b3815652576f5974c04
Patriciadesu <118905032+Patriciadesu@users.noreply.github.com>
2024-04-15 15:27:09 +0700

    update

---
+++ SiAI_Assistance/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SiAI_Assistance
+//
+//  Created by Pataranan Sethpakdee on 15/4/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SiAI_Assistance/SiAI_AssistanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SiAI_AssistanceApp.swift
+//  SiAI_Assistance
+//
+//  Created by Pataranan Sethpakdee on 15/4/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct SiAI_AssistanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
